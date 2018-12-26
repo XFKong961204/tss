1924ee6d0863c815f8821294d3b77b4dad6a1a18
xfkong <xfkong961204@163.com>
2018-12-26 10:34:29 +0800

    增加测试类

---
+++ src/test/java/DevTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("classpath:spring.xml")
+public class DevTest {
+    @Test
+    public void test(){
+
+    }
+}
